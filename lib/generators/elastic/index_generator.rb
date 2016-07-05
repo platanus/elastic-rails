7924930b6da4a83ecc29160510963cbdd2eff84f
Ignacio Baixas <iobaixas@gmail.com>
2016-07-05 10:45:00 -0400

    chore(): renames generator target directory to 'indices'

--- lib/generators/elastic/index_generator.rb
+++ lib/generators/elastic/index_generator.rb
@@ -2,9 +2,9 @@ module Elastic
   class IndexGenerator < Rails::Generators::NamedBase
     source_root File.expand_path("../templates", __FILE__)
 
-    desc "This generator creates a new model index definition at app/indexes"
+    desc "This generator creates a new model index definition at app/indices"
     def create_index
-      template('index.rb', "app/indexes/#{file_name.underscore}_index.rb")
+      template('index.rb', "app/indices/#{file_name.underscore}_index.rb")
     end
   end
 end
