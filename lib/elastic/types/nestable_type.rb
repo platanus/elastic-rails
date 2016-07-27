af3c38a6d954dfe52fa9a85318e3617e060f6f56
Ignacio Baixas <iobaixas@gmail.com>
2016-07-27 17:43:06 -0400

    feat(nestable_type): adds target option to nested
    Also makes method infer target based on property name

--- lib/elastic/types/nestable_type.rb
+++ lib/elastic/types/nestable_type.rb
@@ -1,7 +1,11 @@
 module Elastic::Types
   module NestableType
-    def nested(_name, using: nil, &_block)
-      using = Class.new(Elastic::NestedType, &_block) unless _block.nil?
+    def nested(_name, using: nil, target: nil, &_block)
+      unless _block.nil?
+        using = Class.new(Elastic::NestedType, &_block)
+        using.target = (target || _name.to_s.singularize.camelize.constantize) rescue nil
+      end
+
       using = (_name + '_index').camelize.constantize if using.nil?
 
       definition.register_field Elastic::Fields::Nested.new(_name, using)
