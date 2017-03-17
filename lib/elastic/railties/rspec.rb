722316e41cf3dba44f733c5e263ec9a545488983
Ignacio Baixas <iobaixas@gmail.com>
2017-03-16 22:08:01 -0300

    fix(railties.rspec): fixes rspec helper not enabling indexing before reseting elastic

--- lib/elastic/railties/rspec.rb
+++ lib/elastic/railties/rspec.rb
@@ -1,10 +1,10 @@
 RSpec.configure do |config|
   config.before(:example) do |example|
     if example.metadata[:elasticsearch]
-      Elastic.drop
-      Elastic.migrate
       Elastic.config.disable_indexing = false
       Elastic.config.disable_index_name_caching = true
+      Elastic.drop
+      Elastic.migrate
     else
       Elastic.config.disable_indexing = true
     end
